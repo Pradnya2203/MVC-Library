f921b2a27631a97f22004a65ebd7b5455f7dcfb5
Pradnya <pradnyashimpi22@gmail.com>
2022-07-17 10:07:10 -0700

    changes in schema.sql

--- schema/schema.sql
+++ schema/schema.sql
@@ -3,9 +3,8 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: db
--- Generation Time: Feb 15, 2020 at 11:43 AM
--- Server version: 5.7.29
--- PHP Version: 7.4.1
+-- Generation Time: Jul 17, 2022
+-- PHP Version: 7.4.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -13,59 +12,45 @@ START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
 --
--- Database: `mvc`
+-- CREATE TABLE FOR client
 --
 
--- --------------------------------------------------------
+CREATE TABLE client (
+  name varchar(255) ,
+  username varchar(255) ,
+  password varchar(255),
+  fine int DEFAULT 0
+);
 
 --
--- Table structure for table `client`
+-- MAKE username AS PRIMARY KEY
 --
-
-CREATE TABLE `client` (
-  `name` varchar(255) ,
-  `username` varchar(255) ,
-  `password` varchar(255),
-  `fine` int SET DEFAULT(0)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+ALTER TABLE client
+  ADD PRIMARY KEY (username);
 
 --
--- Indexes for dumped tables
+-- CREATE TABLE books
+--
+CREATE TABLE books (
+  bookName varchar(255) ,
+  username varchar(255) ,
+  status int DEFAULT 0,
+  issued_on datetime,
+  returned_on datetime
+);
+--
+-- CREATE TABLE Book
 --
 
---
--- Indexes for table `posts`
---
-ALTER TABLE `client`
-  ADD PRIMARY KEY (`username`);
+CREATE TABLE Book (
+  bookName varchar(255) ,
+  number int DEFAULT 0
+);
 
 --
--- AUTO_INCREMENT for dumped tables
---
-CREATE TABLE `books` (
-  `bookName` varchar(255) ,
-  `username` varchar(255) ,
-  `status` int SET DEFAULT(0),
-  `issued_on` datetime,
-  `returned_on` datetime,
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
---
--- AUTO_INCREMENT for table `posts`
+-- MAKE bookName AS PRIMARY KEY
 --
 
-CREATE TABLE `Book` (
-  `bookName` varchar(255) ,
-  `number` int SET DEFAULT(0)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
-ALTER TABLE `Book`
-  ADD PRIMARY KEY (`bookName`);
+ALTER TABLE Book
+  ADD PRIMARY KEY (bookName);
