be50e1b62b4e3847beff1a338811009c39117ebc
Pradnya <pradnyashimpi22@gmail.com>
2022-07-17 23:23:21 -0700

    added insert values in schema.sql

--- schema/schema.sql
+++ schema/schema.sql
@@ -1,15 +1,7 @@
--- phpMyAdmin SQL Dump
--- version 5.0.1
--- https://www.phpmyadmin.net/
---
+
 -- Host: db
 -- Generation Time: Jul 17, 2022
 -- PHP Version: 7.4.3
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
 
 
 --
@@ -29,9 +21,18 @@ CREATE TABLE client (
 ALTER TABLE client
   ADD PRIMARY KEY (username);
 
+INSERT INTO client VALUES('Pradnya','artemis', 'b8dfd29b2dd922fd494fb63a427e519dfaabc4489a4ade985c8eaaaef051a0f52
+72fd75ddda34211039997f8896b5f3693cd92eb75ea2bf786884aa0e31b9b36',0);
+
+INSERT INTO client VALUES('Mayuri','mayuri', ' b8dfd29b2dd922fd494fb63a427e519dfaabc4489a4ade985c8eaaaef051a0f527
+2fd75ddda34211039997f8896b5f3693cd92eb75ea2bf786884aa0e31b9b36',0);
+
 --
 -- CREATE TABLE books
 --
+
+
+
 CREATE TABLE books (
   bookName varchar(255) ,
   username varchar(255) ,
@@ -54,3 +55,13 @@ CREATE TABLE Book (
 
 ALTER TABLE Book
   ADD PRIMARY KEY (bookName);
+
+INSERT INTO Book VALUES('Dune', 9);
+INSERT INTO Book VALUES('Harry Potter', 10);
+INSERT INTO Book VALUES('Paper towns', 8);
+INSERT INTO Book VALUES('Percy Jackson', 13);
+INSERT INTO Book VALUES('Mortal Instruments', 15);
+INSERT INTO Book VALUES('Sapiens', 19);
+INSERT INTO Book VALUES('Maze Runner', 12);
+   
+   
